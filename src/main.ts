708e2a5c57da6b5032e29a0825c3609954715a84
uragana <ucha.janashia@geolab.edu.ge>
2026-07-05 14:50:44 +0400

    სპიიდი

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { inject as injectVercelAnalytics } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
 injectVercelAnalytics();
+injectSpeedInsights();
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
