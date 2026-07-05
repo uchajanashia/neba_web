b17f692a99140cca7929f35f4a0ef2ebd90b3312
uragana <ucha.janashia@geolab.edu.ge>
2026-07-05 14:47:09 +0400

    ანალიტიკა

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { inject as injectVercelAnalytics } from '@vercel/analytics';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+
+injectVercelAnalytics();
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
